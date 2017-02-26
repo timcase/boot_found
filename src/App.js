1e6bd0854e228b2b1b0565734bdf1404fb44cfed
Tim Case <tim@2drops.net>
2017-02-26 09:00:03 -0800

    auto commit

--- src/App.js
+++ src/App.js
@@ -5,19 +5,36 @@ import './App.css';
 class App extends Component {
   render() {
     return (
-      <div className="top-bar">
-        <div className="top-bar-left">
-          <ul className="menu">
-            <li className="menu-text">Booter</li>
-          </ul>
+    <div>
+        <div className="top-bar">
+          <div className="top-bar-left">
+            <ul className="menu">
+              <li className="menu-text"><h1>Booter</h1></li>
+            </ul>
+          </div>
+          <div className="top-bar-right">
+            <ul className="menu">
+              <li><a href="#">Signup</a></li>
+              <li><a href="#">Login</a></li>
+            </ul>
+          </div>
         </div>
-        <div className="top-bar-right">
-          <ul className="menu">
-            <li><a href="#">Signup</a></li>
-            <li><a href="#">Login</a></li>
-          </ul>
+        <div className="row">
+          <div className="large-12 columns">
+            <div className="callout">
+              <h2>Todo so cool, so list</h2>
+              <p>This is a magic web app</p>
+            </div>
+          </div>
         </div>
-      </div>
+        <div className="row">
+          <div className="large-12 columns">
+            <div className="footer">
+              <p>content</p>
+            </div>
+          </div>
+        </div>
+    </div>
     );
   }
 }
