83b75459bc556ade1c58941e8b0394cee865e70f
Tim Case <tim@2drops.net>
2017-03-04 08:29:08 -0800

    Add jquery

--- src/App.js
+++ src/App.js
@@ -1,8 +1,15 @@
 import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
 import './App.css';
 import { Link } from 'react-router';
+import $ from 'jquery';
 
 class App extends Component {
+  handleClick = () => {
+    const $modal = $(ReactDOM.findDOMNode(this.refs.exampleModal1));
+    $modal.foundation('open');
+  }
+
   render() {
     return (
     <div>
@@ -14,9 +21,18 @@ class App extends Component {
           </div>
           <div className="top-bar-right">
             <ul className="menu">
+              <li><a onClick={this.handleClick} >Modal</a></li>
               <li><Link to='/signup'>Sign up</Link></li>
               <li><Link to="/login">Login</Link></li>
             </ul>
+          </div>
+          <div ref="exampleModal1" className="reveal" id="exampleModal1" data-reveal>
+              <h1>Awesome. I Have It.</h1>
+              <p className="lead">Your couch. It is mine.</p>
+              <p>I'm a cool paragraph that lives inside of an even cooler modal. Wins!</p>
+              <button className="close-button" data-close aria-label="Close modal" type="button">
+                <span aria-hidden="true">&times;</span>
+              </button>
           </div>
         </div>
         {this.props.children}
