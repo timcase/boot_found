562bee5f97a1df62989de24df032b84ba6dce95f
Tim Case <tim@2drops.net>
2017-03-04 10:26:44 -0800

    Created form

--- src/ModalForm.js
+++ src/ModalForm.js
@@ -2,28 +2,31 @@ import React, { Component } from 'react';
 
 class ModalForm extends Component {
 
-  get modal(){
-    return window.$('#exampleModal1');
-  }
-
-  handleOpenClick = () => {
-    window.$(document).foundation()
-    this.modal.foundation('open');
-  }
-
-  handleCloseClick = () => {
-    this.modal.foundation('close');
-  }
-
   render() {
     return (
           <div ref="exampleModal1" className="reveal" id="exampleModal1" data-reveal>
-              <h1>Awesome. I Have It.</h1>
-              <p className="lead">Your couch. It is mine.</p>
-              <p>I'm a cool paragraph that lives inside of an even cooler modal. Wins!</p>
-              <button className="close-button" onClick={this.handleCloseClick} aria-label="Close modal" type="button">
-                <span aria-hidden="true">&times;</span>
+              <button className="close-button" aria-label="Close modal" type="button">
+                  <span aria-hidden="true">&times;</span>
               </button>
+              <h4>Create Todo</h4>
+              <form>
+                <label>Description
+                  <input type="text" />
+                </label>
+                <div className="row float-right">
+                  <div className="large-6 columns">
+                    <button className="button secondary">
+                      Cancel
+                    </button>
+                  </div>
+                  <div className="large-6 columns">
+                    <button className="button primary">
+                      Submit
+                    </button>
+                  </div>
+                </div>
+              </form>
+
           </div>
     )
   }
